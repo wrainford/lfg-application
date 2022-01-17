d3caf4181ec81023b7f66888e473411ccd88af28
nabilk11 <khan.nabiln11@gmail.com>
2022-01-16 19:28:25 -0500

    added server template and mongoose connections

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+/* ==== External Modules ==== */
+const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const methodOverride = require('method-override');
+/* ==== Internal Modules ==== */
+const routes = require('./routes/index')
+/* ==== Instanced Modules  ==== */
+const app = express();
+/* ====  Configuration  ==== */
+const PORT = 4000;
+
+app.set('view engine', 'ejs');
+/* ====  Middleware  ==== */
+//body data middleware
+app.use(express.urlencoded({extended: true}));
+//method override middleware
+app.use(methodOverride('_method'));
+//server public files
+app.use(express.static('public'));
+//morgan
+app.use(morgan('tiny'));
+
+/* ====  Routes & Controllers  ==== */
+//Home Route
+app.get("/", (req, res) => {
+    res.render('index')
+});
+//404 Route
+/* ====  Server Listener  ==== */
+
+app.listen(PORT, () => {
+    console.log(`LFG App is connected on port: ${PORT}`)
+});
