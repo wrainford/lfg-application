80e79628e42ceb99ea68200c8191daf00bdc6c0c
PredatorR10 <godofgamingpr10@gmail.com>
2022-01-17 22:57:44 -0500

    created game model

---
+++ models/game.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const gameSchemea = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        releaseDate: {type: Date},
+        genre: {type: String},
+        rating: {type: String},
+        userScore: {type: Number},
+    }
+);
+
+module.exports = mongoose.model("game", gameSchemea);
