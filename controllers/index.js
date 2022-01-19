46da2799022c515f527acf308f0ba76baba3aec9
PredatorR10 <godofgamingpr10@gmail.com>
2022-01-18 20:47:05 -0500

    routed games routes, controllers, and model through index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-users: require('./users')
-
+    users: require('./users'),
+    games: require("./games")
 }
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-    users: require('./users')
+    users: require('./users'),
+    games: require("./games")
 }
