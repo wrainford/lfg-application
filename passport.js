bbebefc388d7c4fddb54417754c6850445b462bc
nabilk11 <khan.nabiln11@gmail.com>
2022-01-20 00:14:16 -0500

    added new passport strategy for local login using bcrypt and passport for encryption

--- passport.js
+++ passport.js
@@ -6,7 +6,28 @@ const bcrypt = require('bcryptjs')
 
 function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
+        const user = await getUserByEmail(email);
+        if (user == null) {
+            return done(null, false, { message: 'Invalid Email - Create an Account!'})
+        }
 
-    }
+        try{
+
+            if(await bcrypt.compare(password, user.password)){
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Password is Incorrect!'})
+            }
+        }catch(err){
+            return done(e)
+        }
+    };
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser));
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser(async (id, done) => {
+        return done(null, await getUserById(id))
+    })
 }
+
+module.exports = initialize;
 
