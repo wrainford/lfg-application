1660e179302ad0ab36c7ebd2c02e5660350ca884
PredatorR10 <godofgamingpr10@gmail.com>
2022-01-20 00:37:45 -0500

    delete method finished for games

--- routes/games.js
+++ routes/games.js
@@ -4,11 +4,9 @@ const controler = require("../controllers");
 router.get("/", controler.games.idx);
 router.get("/new", controler.games.newGame);
 router.get("/:id", controler.games.show);
-
 router.post("/", controler.games.create);
-router.get("/:id/edit", controler.games.edit);
 router.put("/:id", controler.games.update);
 router.delete("/:id", controler.games.destroy);
-
+router.get("/:id/edit", controler.games.edit);
 
 module.exports = router;
